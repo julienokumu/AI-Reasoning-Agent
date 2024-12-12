01fa4dc766d7b44cff349cdbf76701c994cc248c
julienokumu <codewithjulien@gmail.com>
2024-12-12 21:54:52 +0300

    commit

---
+++ reasoning_agent.py
@@ -0,0 +1,28 @@
+from phi.agent import Agent
+from phi.model.openai import OpenAIChat
+import os
+
+api_key=os.environ["API_KEY"]
+
+reasoning_agent = Agent(
+    name="Reasoning Agent",
+    model=OpenAIChat(
+        id="gpt-4o",
+        base_url="https://models.inference.ai.azure.com",
+        api_key=api_key
+    ),
+    markdown=True,
+    reasoning=True,
+    structured_outputs=True,
+    instructions=[
+        "Start by giving a philosophical quote by Franz Kafka in relation to the question"
+    ]
+)
+
+task = (
+    "There are 4 rats in a kitchen"
+    "Safely remove all 4 rats from the kitchen without alerting the other rats"
+    "Provide a step-by-step solution and show the solution as an ASCII diagram"
+)
+
+reasoning_agent.print_response(task, stream=True, show_full_reasoning=True)
